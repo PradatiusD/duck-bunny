850f0906ed888cc87ba2bc6ec42bec8b7030360a
PradatiusD <danielprada2012@gmail.com>
2013-12-22 10:22:28 -0500

    Added original sidr load script

---
+++ sidr-load.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+    jQuery('#responsive-menu-button').sidr({
+      name: 'sidr-main',
+      source: 'http://theduck.wpengine.com/wp-content/themes/the-duck-v3/responsive-menu.html'
+    });
+});
+
