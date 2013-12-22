7d71e91ffce086fc889d1fc82efd833b84b9f2b5
PradatiusD <danielprada2012@gmail.com>
2013-12-22 10:33:44 -0500

    Solved button opening and closing issue

--- sidr-load.js
+++ sidr-load.js
@@ -1,7 +1,17 @@
-jQuery(document).ready(function() {
-    jQuery('#responsive-menu-button').sidr({
-      name: 'sidr-main',
-      source: 'http://theduck.wpengine.com/wp-content/themes/the-duck-v3/responsive-menu.html'
-    });
-});
+jQuery(document).ready(function($) {
 
+	var respButton = $('#responsive-menu-button').parent();
+
+    respButton.sidr({
+		name: 'sidr-main',
+		source: 'http://theduck.wpengine.com/wp-content/themes/the-duck-v3/responsive-menu.html'
+    });
+
+	$('#head, .site-container').click(function(){
+
+		$.sidr('close', 'sidr-main');
+
+	});
+
+
+});
